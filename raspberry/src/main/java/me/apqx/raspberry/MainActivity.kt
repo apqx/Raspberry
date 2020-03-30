a85a50eb0031e0b366957791e1ed21598dff1a2c
apqx <changgongapq@gmail.com>
2020-03-31 00:18:42 +0800

    MainActivity

--- raspberry/src/main/java/me/apqx/raspberry/MainActivity.kt
+++ raspberry/src/main/java/me/apqx/raspberry/MainActivity.kt
@@ -11,20 +11,6 @@ import me.apqx.jetlog.LogUtil
 import me.apqx.raspberry.receiver.WifiStateReceiver
 import me.apqx.raspberry.utils.WifiUtils
 
-/**
- * Skeleton of an Android Things activity.
- *
- * Android Things peripheral APIs are accessible through the PeripheralManager
- * For example, the snippet below will open a GPIO pin and set it to HIGH:
- *
- * val manager = PeripheralManager.getInstance()
- * val gpio = manager.openGpio("BCM6").apply {
- *     setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
- * }
- * gpio.value = true
- *
- * You can find additional examples on GitHub: https://github.com/androidthings
- */
 class MainActivity : AppCompatActivity(), ILogUIListener {
     private lateinit var wifiManager: WifiManager
     private lateinit var wifiStateReceiver: WifiStateReceiver
